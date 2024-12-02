ab3e81e52df6914f80f68e4f444fcd54fa9b1466
Ahmet Taha Kahya <taha.kahya@sabanciuniv.edu>
2024-12-02 15:42:10 +0300

    Add text to landing page

--- components/landingScreen/index.js
+++ components/landingScreen/index.js
@@ -3,16 +3,24 @@ import { Grid, Icon } from 'semantic-ui-react'
 import GlobeNetworkSvg from '@components/globeNetworkSvg'
 import LandingIcon from '@components/landingIcon'
 import styles from './landingScreen.module.css'
+import Link from 'next/link'
 
 const LandingScreen = ({ onScroll }) => {
 	return (
 		<Grid>
 			<Grid.Row className={styles.headerRow}>
-				<Grid.Column width={10} textAlign='center' verticalAlign='middle'>
-					<div className={styles.landingText}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec justo
-						massa, facilisis sit amet sem ut, fringilla pharetra velit. Ut
-						tristique ligula sed nisi luctus
+				<Grid.Column width={10} textAlign='center' verticalAlign='middle' >
+					<div className={styles.landingText} style={{ padding: '0 4rem', color: 'black'}}>
+						<Link href="/">
+							<span 
+								style={{ color: 'rgb(25, 120, 120)', cursor: 'pointer' }}
+							>
+								DiplomacyNet
+							</span>
+						</Link> project gathers reports and news for nearly 200 countries globally. Using NLP and Network Science, we map the interactions topically and longitudinally.
+					</div>
+					<div className={styles.descriptionText} style={{ padding: '0 4rem', marginTop: '2rem', fontSize: '20px', color: 'gray', textAlign: 'left', lineHeight: '1.6'}}>
+						We scrape websites of Executive and Ministry of Foreign Affairs of these countries to collect reports and news.
 					</div>
 				</Grid.Column>
 				<Grid.Column width={6} textAlign='center' verticalAlign='middle'>
@@ -22,32 +30,33 @@ const LandingScreen = ({ onScroll }) => {
 
 			<Grid.Row className={styles.infoRow}>
 				<Grid.Column width={16} className={styles.iconsContainer}>
-					<div className={styles.iconsWrapper}>
+					<div className={styles.iconsWrapper} style={{ display: 'flex', alignItems: 'flex-start' }}>
 						<LandingIcon
 							icon='flag'
-							desc='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse viverra.'
+							desc='Thousand of articles spanning more than 20 years'
 						/>
 						<LandingIcon
 							icon='globe'
-							desc='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse viverra.'
+							desc='Interaction between countries mapped globally by extraction mentions of countries and politicians.'
 						/>
 						<LandingIcon
 							icon='map outline'
-							desc='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse viverra.'
+							desc='We offer detailed interactive tool for you to inspecting data.'
 						/>
 						<LandingIcon
 							icon='balance scale'
-							desc='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse viverra.'
+							desc='Country-level insights extracted for topic of text and visuals shared online.'
 						/>
 					</div>
 				</Grid.Column>
 			</Grid.Row>
 
 			<Grid.Row className={styles.scrolRow}>
-				<Grid.Column width={16} textAlign='center' verticalAlign='top'>
+				<Grid.Column width={16} style={{ marginTop: '3rem', marginBottom: '1rem' }} textAlign='center' verticalAlign='top'>
 					<Icon
 						name='angle double down'
 						size='huge'
+						color='rgb(25, 120, 120)'
 						className={styles.scrollIcon}
 						onClick={onScroll}
 					/>
