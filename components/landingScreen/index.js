31d831b193bcec80dc4208e6fbca674745447867
Ahmet Taha Kahya <taha.kahya@sabanciuniv.edu>
2025-04-28 02:12:00 +0200

    Add dataset links to landing page

--- components/landingScreen/index.js
+++ components/landingScreen/index.js
@@ -8,6 +8,30 @@ import Link from 'next/link'
 const LandingScreen = ({ onScroll }) => {
 	return (
 		<Grid>
+			<Grid.Row className={styles.datasetRow}>
+				<Grid.Column width={16} textAlign='center' verticalAlign='middle'>
+					<div className={styles.datasetContainer} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '2rem', padding: '0 6rem'}}>
+						<div className={styles.datasetText} style={{ fontSize: '16px', color: 'gray', textAlign: 'left', lineHeight: '1.2'}}>
+							Our <span style={{ color: 'rgb(25, 120, 120)', fontWeight: 'bold' }}>dataset</span> and <span style={{ color: 'rgb(25, 120, 120)', fontWeight: 'bold' }}>code</span> are now accessible through the following platforms:
+						</div>
+						<div className={styles.buttonContainer}>
+							<Link href="https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/OFN15B" legacyBehavior>
+								<a className={styles.button} target='_blank' rel='noopener noreferrer'>
+									<img src='/images/logos/harvard-logo.png' alt='dataverse' style={{ width: '24px', height: '24px', marginRight: '8px' }} />
+									Harvard Dataverse
+								</a>
+							</Link>
+							<Link href="https://github.com/ViralLab/GlobalDiplomacyNet-Dataset" legacyBehavior>
+								<a className={styles.button} target='_blank' rel='noopener noreferrer'>
+									<Icon name='github' style={{ fontSize: '24px', marginRight: '8px', marginTop: '2px' }} />
+									GitHub
+								</a>
+							</Link>
+						</div>
+					</div>
+				</Grid.Column>
+			</Grid.Row>
+
 			<Grid.Row className={styles.headerRow}>
 				<Grid.Column width={10} textAlign='center' verticalAlign='middle' >
 					<div className={styles.landingText} style={{ padding: '0 6rem', color: 'black'}}>
@@ -15,9 +39,9 @@ const LandingScreen = ({ onScroll }) => {
 							<span 
 								style={{ color: 'rgb(25, 120, 120)', cursor: 'pointer' }}
 							>
-								DiplomacyNet
+								GlobalDiplomacyNET
 							</span>
-						</Link> project gathers reports and news for nearly 200 countries globally. Using NLP and Network Science, we map the interactions topically and longitudinally.
+						</Link> project gathers reports and news for 160 countries globally. Using NLP and Network Science, we map the interactions topically and longitudinally.
 					</div>
 					<div className={styles.descriptionText} style={{ padding: '0 6rem', marginTop: '2rem', fontSize: '20px', color: 'gray', textAlign: 'left', lineHeight: '1.6'}}>
 						We scrape websites of Executive and Ministry of Foreign Affairs of these countries to collect reports and news.
@@ -27,6 +51,7 @@ const LandingScreen = ({ onScroll }) => {
 					<GlobeNetworkSvg />
 				</Grid.Column>
 			</Grid.Row>
+
 
 			<Grid.Row className={styles.infoRow}>
 				<Grid.Column className={styles.iconsContainer}>
