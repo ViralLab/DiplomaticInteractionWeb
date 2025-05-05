d811f389edb955d80c12d6cf03ad0236d6f3d6ce
Ahmet Taha Kahya <taha.kahya@sabanciuniv.edu>
2025-05-05 02:41:47 +0200

    Add google analytics tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Layout from '@components/layout'
 import '../styles/globals.css'
+import { useGoogleAnalytics } from '../utils/useGoogleAnalytics'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
+	useGoogleAnalytics()
+	
 	return (
 		<>
 			<Head>
@@ -11,6 +15,22 @@ function MyApp({ Component, pageProps }) {
 					href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
 				/>
 			</Head>
+			<Script
+				strategy="afterInteractive"
+				src="https://www.googletagmanager.com/gtag/js?id=G-4QY8YT7STT"
+			/>
+			<Script
+				id="google-analytics"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-4QY8YT7STT');
+					`,
+				}}
+			/>
 			<Layout app={<Component {...pageProps} />} />
 		</>
 	)
